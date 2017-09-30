240afe93a14744130e57990f8283a2724923b487
Derek Allred <derekmallred@gmail.com>
2017-09-30 13:15:31 -0600

    refactored the exports object

--- notesApp/app.js
+++ notesApp/app.js
@@ -5,18 +5,18 @@ const os = require('os');
 const notes = require('./notes');
 const _ = require('lodash');
 
-// let res = notes.addNote();
-// console.log(res);
+let res = notes.addNote();
+console.log(res);
 
-// let sum = notes.add(1,3);
-// console.log(sum);
+let sum = notes.add(1,3);
+console.log(sum);
 
 // let user = os.userInfo();
 
 // fs.appendFileSync('greetings.txt', `Hello ${user.username} you are ${notes.age} years old`);
 
-console.log(_.isString(true));
-console.log(_.isString('Derek'));
+// console.log(_.isString(true));
+// console.log(_.isString('Derek'));
 
-const filteredArray = _.uniq(['Derek','Derek',1,2,3,2,1,5])
-console.log(filteredArray);
+// const filteredArray = _.uniq(['Derek','Derek',1,2,3,2,1,5])
+// console.log(filteredArray);
--- notesApp/notes.js
+++ notesApp/notes.js
@@ -1,8 +1,17 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('added note');
-    return 'New note';
-}
+// module.exports.addNote = () => {
+//     console.log('added note');
+//     return 'New note';
+// }
 
-module.exports.add = (a, b) => a + b;
+// module.exports.add = (a, b) => a + b;
+
+module.exports = {
+    addNote: () => {
+        console.log('added note');
+        return 'New note';
+    },
+    
+    add: (a, b) => a + b,
+};
