2e66d27cfcf61b3435448300b6e954189c6b20b2
Derek Allred <derekmallred@gmail.com>
2017-09-30 13:26:12 -0600

    refactored notes.js function calls

--- notesApp/app.js
+++ notesApp/app.js
@@ -11,12 +11,12 @@ console.log(res);
 let sum = notes.add(1,3);
 console.log(sum);
 
-// let user = os.userInfo();
+let user = os.userInfo();
 
-// fs.appendFileSync('greetings.txt', `Hello ${user.username} you are ${notes.age} years old`);
+fs.appendFileSync('greetings.txt', `Hello ${user.username} you are ${notes.age} years old`);
 
-// console.log(_.isString(true));
-// console.log(_.isString('Derek'));
+console.log(_.isString(true));
+console.log(_.isString('Derek'));
 
-// const filteredArray = _.uniq(['Derek','Derek',1,2,3,2,1,5])
-// console.log(filteredArray);
+const filteredArray = _.uniq(['Derek','Derek',1,2,3,2,1,5])
+console.log(filteredArray);
