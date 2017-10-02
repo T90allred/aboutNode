21eeee78883d8f1ddd55fc7b71f304ebe9120389
Derek Allred <derekmallred@gmail.com>
2017-10-02 10:40:50 -0600

    added command line input functionality

--- notesApp/app.js
+++ notesApp/app.js
@@ -7,4 +7,17 @@ const _ = require('lodash');
 //MY CUSTOME MODULES
 const notes = require('./notes');
 
+let command = process.argv[2];
+console.log(command);
 
+if (command === 'add') {
+    console.log('Adding a new note');
+} else if(command === 'list') {
+    console.log('Listing all notes');
+} else if(command === 'remove') {
+    console.log('Removing a note');
+} else if(command === 'read') {
+    console.log('Reading a note');
+} else {
+    console.log('Command not recognized');
+}
